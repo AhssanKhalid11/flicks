b5e41c48a839d91cbdb128a0b9bbfd156a17d71d
Ahssan Khalid <ahssankhalid11@gmail.com>
2025-02-14 19:21:43 +0000

    now started to use variables in strings

--- flicks.rb
+++ flicks.rb
@@ -1,5 +1,4 @@
-puts "ruby is friendly"
-
-phrase = "ruby is fun"
-
-puts phrase
+title = "Goonies"
+rank = 10
+puts "#{title} has a rank of #{rank}"
+puts "Mikey's favorite movie is #{title}.\nHe gives it a #{rank}."
