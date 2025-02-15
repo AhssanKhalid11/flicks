26f387c6d416fa6ac1b4b7b607ef657860a5000a
Ahssan Khalid <ahssankhalid11@gmail.com>
2025-02-15 08:13:31 +0000

    making methods, and how they interact and how to use them (more than one)

--- flicks.rb
+++ flicks.rb
@@ -1,4 +1,28 @@
-title = "Goonies"
-rank = 10
-puts "#{title} has a rank of #{rank}"
-puts "Mikey's favorite movie is #{title}.\nHe gives it a #{rank}."
+def emoji(emoji = "🤠", number)
+  emoji * number
+end
+
+def movie_info(title, rank = 5)
+  stars = emoji("⭐️", rank)
+  "#{title.capitalize} has a rank of #{rank}: #{stars}"
+end
+
+puts movie_info("goonies", 10)
+puts movie_info("ghostbusters", 9)
+
+movie_title = "goldfinger"
+movie_rank = 8
+puts movie_info(movie_title)
+
+#methods need prameters and must be defined
+#there are two ways to use methods that we have made
+
+#key word prameters 
+#is when a way to make a method that makes you assign variable and value
+#example 
+#  def movie_info(title:, rank:)
+#    stars = "⭐️"
+#    "#{title.capitalize} has a rank of #{rank}: #{stars}"
+#  end
+#
+#  puts movie_info(rank: 10, title: "grinch")
