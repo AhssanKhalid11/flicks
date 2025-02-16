e018e93260314d6b131ed25bb686491bbb9c2c2e
Ahssan Khalid <ahssankhalid11@gmail.com>
2025-02-16 23:51:50 +0000

    conditions. using if + case. + how they work

--- flicks.rb
+++ flicks.rb
@@ -20,12 +20,42 @@ class Movie
     end
 end
 
+movie_1 = Movie.new("the matrix")
 
-movie_1 = Movie.new("goonies", 10)
-puts movie_1.title
-movie_1.title = "goonys"
-puts movie_1.title
-puts movie_1.rank
+number_rolled = rand(1..6)
+
+#in case conditions REMEMBER to change the when 
+
+case number_rolled
+when 1..2 
+  movie_1.thumbs_down
+  puts "#{movie_1.title} got a thumbs down 👎"
+when 3..4
+  puts "#{movie_1.title} got skipped 🤨"
+else
+  movie_1.thumbs_up
+  puts "#{movie_1.title} got a thumbs up 👍"
+end
+
+# if number_rolled < 3
+
+#   movie_1.thumbs_down
+#   puts "#{movie_1.title} got a thumbs down 👎"
+
+# elsif number_rolled < 5
+#   puts "#{movie_1.title} got skipped 🤨"
+
+# else
+#   movie_1.thumbs_up
+#   puts "#{movie_1.title} got a thumbs up 👍"
+
+# end
+
+# movie_1 = Movie.new("goonies", 10)
+# puts movie_1.title
+# movie_1.title = "goonys"
+# puts movie_1.title
+# puts movie_1.rank
 
 # puts movie_1.to_s
 
