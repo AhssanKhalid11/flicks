0e5966c8127d1491f1997388ed0a755938545801
Ahssan Khalid <ahssankhalid11@gmail.com>
2025-02-16 21:18:10 +0000

    applying attr_reader/writer/accessor

--- flicks.rb
+++ flicks.rb
@@ -1,4 +1,7 @@
 class Movie 
+    attr_reader :rank
+    attr_accessor :title
+
     def initialize(title, rank = 5)
         @title = title.capitalize 
         @rank = rank 
@@ -19,15 +22,20 @@ end
 
 
 movie_1 = Movie.new("goonies", 10)
-puts movie_1.to_s
+puts movie_1.title
+movie_1.title = "goonys"
+puts movie_1.title
+puts movie_1.rank
 
-movie_2 = Movie.new("ghostbusters", 9)
-puts movie_2.to_s
+# puts movie_1.to_s
 
-movie_3 = Movie.new("goldfinger")
-puts movie_3.to_s
+# movie_2 = Movie.new("ghostbusters", 9)
+# puts movie_2.to_s
 
-movie_1.thumbs_up
-puts movie_1.to_s
-movie_2.thumbs_down
-puts movie_2.to_s
+# movie_3 = Movie.new("goldfinger")
+# puts movie_3.to_s
+
+# movie_1.thumbs_up
+# puts movie_1.to_s
+# movie_2.thumbs_down
+# puts movie_2.to_s
