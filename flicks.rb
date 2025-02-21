cdb50c61216f592aca593ddc199ad00fda81f029
Ahssan Khalid <ahssankhalid11@gmail.com>
2025-02-21 01:52:48 +0000

    require relative

--- flicks.rb
+++ flicks.rb
@@ -1,68 +1,5 @@
-class Movie 
-    attr_reader :rank
-    attr_accessor :title
-
-    def initialize(title, rank = 5)
-        @title = title.capitalize 
-        @rank = rank 
-    end
-
-    def to_s
-        "#{@title} has a rank of #{@rank}"
-    end
-
-    def thumbs_up
-      @rank += 1
-    end
-
-    def thumbs_down
-      @rank -= 1
-    end
-end
-
-class Playlist
-  attr_reader :name, :movies
-
-  def initialize(name)
-    @name = name.capitalize
-    @movies = []
-  end
-
-  def add_movie(movie)
-    @movies << movie
-  end
-
-  def roll_die
-    rand(1..6)
-  end
-
-  def play
-    puts "*" * 15
-    puts"#{name}'s playlist"
-
-    puts "\nBefore watching:"
-    puts @movies 
-
-    @movies.each do |movie|
-      number_rolled = rand(1..6)
-
-    case number_rolled = roll_die
-      when 1..2 
-        movie.thumbs_down
-        puts "#{movie.title} got a thumbs down 👎"
-      when 3..4
-        puts "#{movie.title} got skipped 🤨"
-      else
-        movie.thumbs_up
-        puts "#{movie.title} got a thumbs up 👍"
-      end
-    end
-
-    puts "\nAfter watching:"
-    puts @movies
-  end
-end
-
+require_relative "lib/flicks/movie"
+require_relative "lib/flicks/playlist"
 
 movie_1 = Movie.new("goonies", 10)
 movie_2 = Movie.new("ghostbusters", 9)
@@ -84,7 +21,9 @@ movie_4 = Movie.new("gremlins", 7)
 playlist_2.add_movie(movie_4)
 playlist_2.play
 
-
+puts "\n"
+puts "new line just to mute and recolour the warning sign"
+puts movies
 
 # if number_rolled < 3
 
@@ -99,4 +38,3 @@ playlist_2.play
 #   puts "#{movie_1.title} got a thumbs up 👍"
 
 # end
-
